381b54d5504359abf3ddcde5e791f17438156db4
Megan Sickinger <mesickinger100@gmail.com>
2021-12-01 16:02:23 -0600

    Add project files.

---
+++ TempConversion/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TempConversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboUnits = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblConversion1Value = new System.Windows.Forms.Label();
+            this.numberboxTempValue = new System.Windows.Forms.NumericUpDown();
+            this.lblConversion2Value = new System.Windows.Forms.Label();
+            this.ImageTempGauge = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numberboxTempValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageTempGauge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboUnits
+            // 
+            this.comboUnits.Font = new System.Drawing.Font("Courier New", 12F);
+            this.comboUnits.FormattingEnabled = true;
+            this.comboUnits.Items.AddRange(new object[] {
+            "Fahrenheit",
+            "Celsius",
+            "Kelvin"});
+            this.comboUnits.Location = new System.Drawing.Point(667, 71);
+            this.comboUnits.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.comboUnits.Name = "comboUnits";
+            this.comboUnits.Size = new System.Drawing.Size(199, 26);
+            this.comboUnits.TabIndex = 0;
+            this.comboUnits.SelectedIndexChanged += new System.EventHandler(this.comboUnits_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F);
+            this.label1.Location = new System.Drawing.Point(20, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Starting Temperature:";
+            // 
+            // lblConversion1Value
+            // 
+            this.lblConversion1Value.AutoSize = true;
+            this.lblConversion1Value.Font = new System.Drawing.Font("Courier New", 12F);
+            this.lblConversion1Value.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblConversion1Value.Location = new System.Drawing.Point(662, 152);
+            this.lblConversion1Value.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblConversion1Value.Name = "lblConversion1Value";
+            this.lblConversion1Value.Size = new System.Drawing.Size(68, 18);
+            this.lblConversion1Value.TabIndex = 2;
+            this.lblConversion1Value.Text = "value1";
+            this.lblConversion1Value.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numberboxTempValue
+            // 
+            this.numberboxTempValue.Font = new System.Drawing.Font("Courier New", 12F);
+            this.numberboxTempValue.Location = new System.Drawing.Point(433, 71);
+            this.numberboxTempValue.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.numberboxTempValue.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numberboxTempValue.Minimum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            -2147483648});
+            this.numberboxTempValue.Name = "numberboxTempValue";
+            this.numberboxTempValue.Size = new System.Drawing.Size(200, 26);
+            this.numberboxTempValue.TabIndex = 3;
+            this.numberboxTempValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numberboxTempValue.ValueChanged += new System.EventHandler(this.numberboxTempValue_ValueChanged);
+            // 
+            // lblConversion2Value
+            // 
+            this.lblConversion2Value.AutoSize = true;
+            this.lblConversion2Value.Font = new System.Drawing.Font("Courier New", 12F);
+            this.lblConversion2Value.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblConversion2Value.Location = new System.Drawing.Point(662, 226);
+            this.lblConversion2Value.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblConversion2Value.Name = "lblConversion2Value";
+            this.lblConversion2Value.Size = new System.Drawing.Size(68, 18);
+            this.lblConversion2Value.TabIndex = 4;
+            this.lblConversion2Value.Text = "value2";
+            this.lblConversion2Value.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // ImageTempGauge
+            // 
+            this.ImageTempGauge.Location = new System.Drawing.Point(25, 126);
+            this.ImageTempGauge.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.ImageTempGauge.Name = "ImageTempGauge";
+            this.ImageTempGauge.Size = new System.Drawing.Size(358, 141);
+            this.ImageTempGauge.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.ImageTempGauge.TabIndex = 5;
+            this.ImageTempGauge.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 302);
+            this.Controls.Add(this.ImageTempGauge);
+            this.Controls.Add(this.lblConversion2Value);
+            this.Controls.Add(this.numberboxTempValue);
+            this.Controls.Add(this.lblConversion1Value);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboUnits);
+            this.Font = new System.Drawing.Font("Courier New", 12F);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Temperature Conversion Program";
+            ((System.ComponentModel.ISupportInitialize)(this.numberboxTempValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageTempGauge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboUnits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblConversion1Value;
+        private System.Windows.Forms.NumericUpDown numberboxTempValue;
+        private System.Windows.Forms.Label lblConversion2Value;
+        private System.Windows.Forms.PictureBox ImageTempGauge;
+    }
+}
+
---
+++ TempConversion/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ImageTempGauge.Image = TempConversion.Properties.Resources.temperaturegauge_none;
+        }
+
+        decimal temp;
+       
+        private void numberboxTempValue_ValueChanged(object sender, EventArgs e)
+        {
+            temp = numberboxTempValue.Value;
+            if (comboUnits.SelectedItem.ToString() == "Fahrenheit")
+            {
+                convertF(temp);
+               
+            }
+           else if (comboUnits.SelectedItem.ToString() == "Celsius")
+            {
+                convertC(temp);
+            }
+           else if( comboUnits.SelectedItem.ToString() == "Kelvin")
+            {
+                try
+                {
+                    if (temp >= 0)
+                        convertK(temp);
+                    else if (temp < 0)
+                    {
+                        throw new Exception("Kelvin Temperature cannot be below 0");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    numberboxTempValue.Value = 0;
+                }
+            }
+            
+        }
+
+        private void comboUnits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void convertF(decimal temp)
+        {
+            decimal convertToC;
+            decimal convertToK;
+
+            convertToC = (5/9)* (temp - 32);
+            convertToK = convertToC+273.15M;
+
+            lblConversion1Value.Text = convertToC.ToString() + '°' + 'C';
+            lblConversion2Value.Text = convertToK.ToString() + '°' + 'K';
+
+            changeTempGauge(temp);
+        }
+
+        public void convertC(decimal temp)
+        {
+            decimal convertToF;
+            decimal convertToK;
+            
+            convertToK = temp + 273.15M;
+            convertToF = temp * (9 / 5) + 32;
+
+            lblConversion1Value.Text = convertToF.ToString() + '°' + 'F';
+            lblConversion2Value.Text = convertToK.ToString() + '°' + 'K';
+
+            changeTempGauge(convertToF);
+        }
+        public void convertK(decimal temp)
+        {
+            decimal convertToF;
+           
+            decimal convertToC;
+
+            
+
+            convertToF = (temp - 273.15M) * (9 / 5) + 32;
+            convertToC = temp - 273.15M;
+
+            lblConversion1Value.Text =convertToF.ToString() + '°'+'F';
+            lblConversion2Value.Text = convertToC.ToString() + '°'+'C';
+
+            changeTempGauge(convertToF);
+
+        }
+
+        public void changeTempGauge(decimal tempF)
+        {
+            if (tempF > 95 )
+            {
+                ImageTempGauge.Image = TempConversion.Properties.Resources.temperaturegauge_hot;
+            }
+            else if(tempF <=95 && tempF > 75)
+            {
+                ImageTempGauge.Image = TempConversion.Properties.Resources.temperaturegauge_warm;
+            }
+            else if( tempF <=75 && tempF >= 55)
+            {
+                ImageTempGauge.Image = TempConversion.Properties.Resources.temperaturegauge_mild;
+            }
+            else if(tempF <55 && tempF >=33)
+            {
+                ImageTempGauge.Image = TempConversion.Properties.Resources.temperaturegauge_cold;
+            }
+            else if(tempF <33 )
+            {
+                ImageTempGauge.Image = TempConversion.Properties.Resources.temperaturegauge_freezing;
+            }
+            //else if(numberboxTempValue.Value==null )
+           // {
+            //    ImageTempGauge.Image = TempConversion.Properties.Resources.temperaturegauge_none;
+            //}
+        }
+    }
+}
